1a8a0750428a87294662e940c577382fa80995a7
Sebastian Karlsen <sebastian@karlsen.fr>
2022-03-22 02:08:53 +0100

    Patch: alpha + escape sequence to change background color
    "If you have st with the alpha patch and you change the background color using an escape sequence then the transparency is lost and the terminal becomes opaque. This patch solved this."
    
    - https://www.reddit.com/r/suckless/comments/mwt3to/st_alpha_escape_sequence_to_change_background/

--- x.c
+++ x.c
@@ -856,6 +856,14 @@ xsetcolorname(int x, const char *name)
 	XftColorFree(xw.dpy, xw.vis, xw.cmap, &dc.col[x]);
 	dc.col[x] = ncolor;
 
+    if (x == defaultbg) {
+        if (opt_alpha)
+            alpha = strtof(opt_alpha, NULL);
+        dc.col[defaultbg].color.alpha = (unsigned short)(0xffff * alpha);
+        dc.col[defaultbg].pixel &= 0x00FFFFFF;
+        dc.col[defaultbg].pixel |= (unsigned char)(0xff * alpha) << 24;
+    }
+
 	return 0;
 }
 
